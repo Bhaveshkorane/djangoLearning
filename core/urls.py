d8710e4925699347599c3e3ac243774b5c550991
bhaveshkorane <bhavesh.korane@tekditechnologies.com>
2024-07-03 14:35:49 +0530

    this is first commit

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
+    path('',hello),
+    path('aboutme/',aboutme),
+    path('my-resume',myresume),
+    path('fetchData/',fetchData),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,30 @@
+from multiprocessing import context
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("this is my server")
+
+def aboutme(request):
+    return render(request,"aboutme.html")
+
+def myresume(request):
+    return render(request,"my_resume.html")
+
+
+text="""hello bhavesh this is your text that you are fetchning form the backend"""
+
+student=[
+    {'name':'Bhavesh', 'age':30},
+    {'name':'Abhishek', 'age':20},
+    {'name':'Sainath', 'age':15},
+    {'name':'Adinath', 'age':25}
+]
+
+
+
+def fetchData(request):
+    return render(request,"fetchData.html",context={'text':text,'student':student})
+
+
+
